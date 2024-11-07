a2735a83bf5f97110787ac049d5d7ad2e938dbf7
milamailo <miila.maiilo@gmail.com>
2024-11-07 15:41:55 -0500

    questions

--- script_q_01.sql
+++ script_q_01.sql
@@ -1,10 +1,11 @@
--- List all posts made by a user, including the number of reactions each post has received.
+-- List all posts made by a user, including the number of reactions 
+-- each post has received.
 use socialmediadb;
 
-set @uid = 12;
+set @uid = 1;
 
-SELECT Posts.PostContent, COUNT(Reactions.ReactionID) AS ReactionCount
+SELECT Posts.PostID, Posts.PostContent, COUNT(Reactions.ReactionID) AS ReactionCount
 FROM Posts
 LEFT JOIN Reactions ON Posts.PostID = Reactions.PostID
 WHERE Posts.UserID = @uid
-GROUP BY Posts.PostID;
+Group by Posts.PostID
