62e2f9ab3d63764a2ef5fdef2192f3ba5111535d
milamailo <miila.maiilo@gmail.com>
2024-11-11 12:03:48 -0500

    remove the questions

--- script_q_03.sql
+++
@@ -1,9 +0,0 @@
--- Find all comments on a specific post.
-use socialmediadb;
-
-set @pid = 1;
-
-SELECT Comments.CommentContent, Users.Username
-FROM Comments
-JOIN Users ON Comments.UserID = Users.UserID
-WHERE Comments.PostID = @pid;
